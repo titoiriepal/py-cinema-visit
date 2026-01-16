7f6b1dfc0951f578625bbb406915674e1309a9f4
titoiriepal <titoiriepal@hotmail.com>
2026-01-16 11:49:14 +0100

    Changed requested from pull request added

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -2,9 +2,7 @@ from app.people.customer import Customer
 
 
 class CinemaBar:
-    def __init__(self) -> None:
-        pass
 
     @staticmethod
-    def sell_product(customer: Customer, product: str) -> str:
-        print(f"Cinema bar sold {product} to {customer.name}")
+    def sell_product(customer: Customer, product: str) -> None:
+        print(f"Cinema bar sold {product} to {customer.name}.")
--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -7,10 +7,10 @@ class CinemaHall:
         self.number = number
 
     def movie_session(
-        self, movie_name: str, customers: list[Customer], clean_staff: Cleaner
+        self, movie_name: str, customers: list[Customer], cleaning_staff: Cleaner
     ) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
-            print(customer.watch_movie(movie_name))
+            customer.watch_movie(movie_name)
         print(f'"{movie_name}" ended.')
-        clean_staff.clean_hall(self.number)
+        cleaning_staff.clean_hall(self.number)
